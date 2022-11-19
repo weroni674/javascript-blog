4b27272d0500602b2df74d1662991423f0fa2d1e
Weronika Malinowska <weronika.malinowska36@gmail.com>
2022-11-19 21:28:32 +0100

    Changaes at js script

--- js/script.js
+++ js/script.js
@@ -10,12 +10,16 @@
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
+    for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
-}
+    }
   
     /* add class 'active' to the clicked link */
-  
+    console.log('clickedElement:', clickedElement);  
+    event.preventDefault();
+    const clickedElement = this;
+    clickedElement.classList.add('active');
+
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('post');
 
@@ -24,9 +28,10 @@ for(let activeLink of activeLinks){
     }
   
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector= clickedElement.getAttribute(href);
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle= dokument.querySelector('href');
     /* add class 'active' to the correct article */
   }
   
