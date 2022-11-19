e51e4e1010a2aea7b4c17c0bad406dd50b3e8f49
Weronika Malinowska <weronika.malinowska36@gmail.com>
2022-11-19 20:42:54 +0100

    Add class of article

--- js/script.js
+++ js/script.js
@@ -5,13 +5,23 @@
   const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
-    
+
   
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('post');
+
+    for(let activeArticle of activeLinks){
+    activeLink.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
