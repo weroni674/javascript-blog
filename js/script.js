4ce3f23513c522b761415a26ab07c2864706bab5
Weronika Malinowska <weronika.malinowska36@gmail.com>
2022-11-22 21:23:18 +0100

    Add new code

--- js/script.js
+++ js/script.js
@@ -1,45 +1,35 @@
-/**document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });**/
-  const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
+const titleClickHandler = function(event){
+  event.preventDefault();
+  const clickedElement = this;
 
-  
-    /* remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll('.titles a.active');
-
-    for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-    }
-  
-    /* add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);  
-    event.preventDefault();
-    const clickedElement = this;
-    clickedElement.classList.add('active');
-
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('post');
-
-    for(let activeArticle of activeLinks){
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-    }
-  
-    /* get 'href' attribute from the clicked link */
-    const articleSelector= clickedElement.getAttribute(href);
+  }
 
-    /* find the correct article using the selector (value of 'href' attribute) */
-    const targetArticle= dokument.querySelector('href');
-    /* add class 'active' to the correct article */
+  /* add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post.active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
   }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
-  /*Nie rozumiem logiki js, i po kolei jak się tworzy stałe i zmienne odwołania do poszczególnych elementow html*/
-  /*potrzebuje wytlumaczenia jaka funkcja co robi co zwraca i jak sie czego uzywa*/
-  
+
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute(href);
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  targetArticle.classList.add('active');
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+
+/*Nie rozumiem logiki js, i po kolei jak się tworzy stałe i zmienne odwołania do poszczególnych elementow html*/
+/*potrzebuje wytlumaczenia jaka funkcja co robi co zwraca i jak sie czego uzywa*/
